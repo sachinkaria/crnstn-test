115a2b10438da9dae95039341ceb57627d4f4a21
Sachin Karia <sachinkaria@sachins-mbp-2.home>
2019-08-11 22:09:40 +0100

    touch up

--- src/components/BeerListItem/BeerListItem.js
+++ src/components/BeerListItem/BeerListItem.js
@@ -6,13 +6,13 @@ import './BeerListItem.css'
 function BeerListItem(props) {
     return (
         <Col className="beer-list-item" xs="12" sm="6" md="4" lg="3">
-            <div className="beer-list-item-details" style={{marginBottom: '20px'}}>
-                <h4 style={{fontWeight: 'bold'}}>{props.name}</h4>
+            <div className="beer-list-item-details">
+                <h4 className="beer-name">{props.name}</h4>
                 <span>ABV: {props.abv}%</span>
             </div>
             <img className="beer-list-item-image" src={props.image_url} alt={props.name}/>
             <Link to={`/beer/${props.id}`} className="beer-list-item-link">
-            <span>More details</span>
+            <span className="beer-details-link">More details</span>
             </Link>
         </Col>
     )
